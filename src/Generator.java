87a5fdc36628015ed90b78498e440b79612d95b1
Girin Ilya <39518953+Choos37ricK@users.noreply.github.com>
2019-12-27 19:56:06 +0300

    step 2

---
+++ src/Generator.java
@@ -0,0 +1,36 @@
+public class Generator {
+
+    private static char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+
+    public static String numbersGenerator(int regionCode) {
+        StringBuffer buffer = null;
+        for (int number = 1; number < 1000; number++) {
+            StringBuilder builder = new StringBuilder();
+            buffer = new StringBuffer();
+            for (char firstLetter : letters) {
+                for (char secondLetter : letters) {
+                    for (char thirdLetter : letters) {
+                        builder.append(firstLetter);
+                        builder.append(padNumber(number, 3));
+                        builder.append(secondLetter);
+                        builder.append(thirdLetter);
+                        builder.append(padNumber(regionCode, 2));
+                        builder.append("\n");
+
+                    }
+                }
+            }
+        }
+        return buffer.toString();
+    }
+
+    private static String padNumber(int number, int numberLength)
+    {
+        StringBuilder numberStr = new StringBuilder(Integer.toString(number));
+        int padSize = numberLength - numberStr.length();
+        for(int i = 0; i < padSize; i++) {
+            numberStr.insert(0, '0');
+        }
+        return numberStr.toString();
+    }
+}
--- src/Loader.java
+++ src/Loader.java
@@ -1,5 +1,4 @@
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Loader
 {
@@ -7,43 +6,19 @@ public class Loader
     {
         long start = System.currentTimeMillis();
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        //FileOutputStream writer = new FileOutputStream("res/numbers.txt");
         //PrintWriter writer = new PrintWriter("res/numbers.txt");
+        File file = new File("res/numbers.txt");
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+
         for (int regionCode = 1; regionCode < 100; regionCode++) {
-            for (int number = 1; number < 1000; number++) {
-                StringBuilder builder = new StringBuilder();
-                StringBuffer buffer = new StringBuffer();
-                for (char firstLetter : letters) {
-                    for (char secondLetter : letters) {
-                        for (char thirdLetter : letters) {
-                            builder.append(firstLetter);
-                            builder.append(padNumber(number, 3));
-                            builder.append(secondLetter);
-                            builder.append(thirdLetter);
-                            builder.append(padNumber(regionCode, 2));
-                            builder.append("\n");
-                        }
-                    }
-                }
-                //writer.write(builder.toString());
-                writer.write(builder.toString().getBytes());
-            }
+            Generator.numbersGenerator(regionCode);
         }
         writer.flush();
         writer.close();
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
-    }
-
-    private static String padNumber(int number, int numberLength)
-    {
-        StringBuilder numberStr = new StringBuilder(Integer.toString(number));
-        int padSize = numberLength - numberStr.length();
-        for(int i = 0; i < padSize; i++) {
-            numberStr.insert(0, '0');
-        }
-        return numberStr.toString();
     }
 }
