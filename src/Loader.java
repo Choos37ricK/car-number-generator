34abc369001ddcc0105d4b8caf123e1e1f2e92f3
Girin Ilya <39518953+Choos37ricK@users.noreply.github.com>
2020-01-08 10:42:20 +0300

    added mutlifile generation

--- src/Loader.java
+++ src/Loader.java
@@ -1,21 +1,49 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Loader
 {
+    static int countOfRegion = 12;
+    static int countOfThread = 4;
+
     public static void main(String[] args) throws Exception
     {
+        System.out.println("One file generation time:");
+        long avgTime;
+        long sum = 0;
+        for (int i = 0; i < 3; i++) {
+            long returnedTime = oneFileGeneration();
+            System.out.println(returnedTime + " ms");
+            sum += returnedTime;
+        }
+        avgTime = sum / 3;
+        System.out.println("Average time: " + avgTime + " ms");
+
+        System.out.println("\n\nMulti file generation time:");
+        sum = 0;
+        for (int i = 0; i < 3; i++) {
+            long returnedTime = multiFileGeneration();
+            System.out.println(returnedTime + " ms");
+            sum += returnedTime;
+        }
+        avgTime = sum / 3;
+        System.out.println("Average time: " + avgTime + " ms");
+
+        System.out.println("\nMulti file generation always is faster");
+    }
+
+    private static long oneFileGeneration() throws IOException, ExecutionException, InterruptedException {
         long start = System.currentTimeMillis();
+
         File file = new File("res/numbers.txt");
         FileWriter fileWriter = new FileWriter(file);
         BufferedWriter writer = new BufferedWriter(fileWriter);
 
-        int countOfRegion = 12;
-        int countOfThread = 4;
         int countOfRegionToThread = countOfRegion / countOfThread;
 
         /**
@@ -41,7 +69,7 @@ public class Loader
             }
             int finalI = i;
             /**
-             * Запускаем поток в executor servise
+             * Запускаем поток в executor service
              */
             futures.add(service.submit(() ->
                     new Generator(writer, (finalI - 1) * countOfRegionToThread, finalCountOfRegionToThread).compute()));
@@ -68,6 +96,69 @@ public class Loader
         writer.flush();
         writer.close();
 
-        System.out.println((System.currentTimeMillis() - start) + " ms");
+        long executionTime = System.currentTimeMillis() - start;
+        return executionTime;
+    }
+
+    private static long multiFileGeneration() throws IOException, ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
+
+        int countOfRegionToThread = countOfRegion / countOfThread;
+
+        /**
+         * ExecutorService класс для пула потоков. Количество потоков выполняемых
+         * в один момент времени строго фиксировано. (Количество указывается в newFixedThreadPool)
+         */
+        ExecutorService service = Executors.newFixedThreadPool(countOfThread);
+
+        /**
+         * Future класс, который хранит в себе состояние потока.
+         * При запуске потока в ExecutorService, он возвращает объект класса Future.
+         * Нам он необходим в данном коде для ожидания конца всех потоков.
+         *
+         * В коллекции содержатся все future для каждого потока.
+         */
+        List<Future> futures = new ArrayList<>();
+        for (int i = 1; i <= countOfThread; i++) {
+            int finalCountOfRegionToThread;
+            if (i == countOfThread) {
+                finalCountOfRegionToThread = countOfRegion - (countOfRegionToThread * (i - 1));
+            } else {
+                finalCountOfRegionToThread = countOfRegionToThread;
+            }
+            int finalI = i;
+            /**
+             * Запускаем поток в executor service
+             */
+            futures.add(service.submit(() ->
+            {
+                try {
+                    new MultifileGenerator((finalI - 1) * countOfRegionToThread, finalCountOfRegionToThread).compute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }));
+        }
+
+        /**
+         * Данный метод говорит сервису прекратить
+         * ожидание новых потоков после окончания всех запущенных.
+         * Необходим для того, чтобы программа, после окончания метода main,
+         * не жадала ещё чего-нибудь и прекратила работу.
+         */
+        service.shutdown();
+
+        /**
+         * В данном цикле мы ожидаем, пока все потоки закончат работу.
+         * Метод get еобходим для возврата значения из потока.
+         * Но так как чтобы вернуть значение, он дожидается его
+         * [потока] конца, мы используем его для ожидания.
+         */
+        for (Future future : futures) {
+            future.get();
+        }
+
+        long executionTime = System.currentTimeMillis() - start;
+        return executionTime;
     }
 }
--- src/MultifileGenerator.java
+++ src/MultifileGenerator.java
@@ -1,22 +1,28 @@
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
-public class Generator {
+public class MultifileGenerator {
 
-    private BufferedWriter writer;
     private static char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+    private File file;
+    private FileWriter fileWriter;
+    private BufferedWriter writer;
     private int regionCodeStart;
     private int regionCodeEnd;
 
-    public Generator(BufferedWriter writer, int regionCodeStart, int regionCodeEnd) {
-        this.writer = writer;
+    public MultifileGenerator(int regionCodeStart, int regionCodeEnd) throws IOException {
         this.regionCodeStart = regionCodeStart + 1;
         this.regionCodeEnd = regionCodeStart + regionCodeEnd;
+        this.file = new File(String.format("%s_%d-%d.txt", "res/numbers_of_regions", this.regionCodeStart, this.regionCodeEnd));
+        this.fileWriter = new FileWriter(this.file);
+        this.writer = new BufferedWriter(this.fileWriter);
     }
 
     protected void compute() {
         StringBuilder stringBuilder;
-        for (int regionCode = regionCodeStart + 1; regionCode <= regionCodeEnd; regionCode++) {
+        for (int regionCode = regionCodeStart; regionCode <= regionCodeEnd; regionCode++) {
             for (int number = 1; number < 1000; number++) {
                 for (char firstLetter : letters) {
                     for (char secondLetter : letters) {
@@ -38,6 +44,12 @@ public class Generator {
                     }
                 }
             }
+        }
+        try {
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.toString());
         }
     }
 
